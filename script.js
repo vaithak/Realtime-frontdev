d8959076a3bbca337b317e021dcddf86e3d0f4e5
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-01-03 14:27:30 +0530

    Add js editor

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var html_editor = document.querySelector('.ht'),
-    css_editor = document.querySelector('.cs');
+var html_editor = document.querySelector('#html'),
+    css_editor = document.querySelector('#css'),
+    js_editor = document.querySelector('#js');
  
-var editors = [html_editor, css_editor];
+var editors = [html_editor, css_editor, js_editor];
  
 editors.forEach(function(editor, i, arr) {
     editor.addEventListener('keyup', function() {
@@ -23,12 +24,14 @@ var base_tpl =
 var prepareSource = function() {
     var html = html_editor.value,
         css = css_editor.value,
+        js=js_editor.value,
         src = '';
     // HTML
     src = base_tpl.replace('</body>', html + '</body>');
     // CSS
     css = '<style>' + css + '</style>';
     src = src.replace('</head>', css + '</head>');
+    js = js.replace('</body>', js + '</body>');
     return src;
 };
 
